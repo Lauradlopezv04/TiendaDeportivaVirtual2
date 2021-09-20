3bbcbddff950704c7311868d29eccfdde4a93ede
Lauradlopezv04 <laurad.lopez0401@gmail.com>
2021-09-20 14:05:21 -0500

    Corrección del orden de las variables en el cliente DAO y VO.
    Corrección nombre clase proveedoresDao por ProveedoresDao

--- TiendaDeportivaVirtual2/src/main/java/com/DAO/TiendaDeportivaVirtual/ClienteDao.java
+++ TiendaDeportivaVirtual2/src/main/java/com/DAO/TiendaDeportivaVirtual/ClienteDao.java
@@ -9,12 +9,12 @@ public class ClienteDao extends Conexion {
 	public void registrarCliente(ClienteVo Cliente) {
 		try {
 			Conectar();
-			PreparedStatement sentencia = Conexion.prepareStatement("insert into clientes(cedula_cliente, direccion_cliente, email_cliente, nombre_cliente, telefono_cliente) values (?,?,?,?,?)");
+			PreparedStatement sentencia = Conexion.prepareStatement("insert into clientes(cedula_cliente, nombre_cliente, direccion_cliente, , telefono_cliente, email_cliente) values (?,?,?,?,?)");
 			sentencia.setLong(1, Cliente.getCedula_cliente());
-			sentencia.setString(2, Cliente.getDireccion_cliente());
-			sentencia.setString(3, Cliente.getEmail_cliente());
-			sentencia.setString(4, Cliente.getNombre_cliente());
-			sentencia.setString(5, Cliente.getTelefono_cliente());
+			sentencia.setString(2, Cliente.getNombre_cliente());
+			sentencia.setString(3, Cliente.getDireccion_cliente());
+			sentencia.setString(4, Cliente.getTelefono_cliente());
+			sentencia.setString(5, Cliente.getEmail_cliente());
 			sentencia.executeUpdate();
 			Desconectar();
 		} catch(Exception e) {
@@ -25,11 +25,11 @@ public class ClienteDao extends Conexion {
 	public void actualizarCliente(ClienteVo Cliente) {
 		try {
 			Conectar();
-			PreparedStatement sentencia = Conexion.prepareStatement("update clientes set direccion_cliente=?, email_cliente = ?, nombre_cliente = ?, telefono_cliente = ? where cedula_cliente = ?");
-			sentencia.setString(1, Cliente.getDireccion_cliente());
-			sentencia.setString(2, Cliente.getEmail_cliente());
-			sentencia.setString(3, Cliente.getNombre_cliente());
-			sentencia.setString(4, Cliente.getTelefono_cliente());
+			PreparedStatement sentencia = Conexion.prepareStatement("update clientes set nombre_cliente = ?, direccion_cliente=?, telefono_cliente = ? email_cliente = ? where cedula_cliente = ?");
+			sentencia.setString(1, Cliente.getNombre_cliente());
+			sentencia.setString(2, Cliente.getDireccion_cliente());
+			sentencia.setString(3, Cliente.getTelefono_cliente());
+			sentencia.setString(4, Cliente.getEmail_cliente());
 			sentencia.setLong(5, Cliente.getCedula_cliente());
 			sentencia.executeUpdate();
 			Desconectar();
@@ -59,10 +59,10 @@ public class ClienteDao extends Conexion {
 			if (datos.next()) {
 				ClienteVo Cliente = new ClienteVo();
 				Cliente.setCedula_cliente(datos.getLong("cedula_cliente"));
-				Cliente.setDireccion_cliente(datos.getString("direccion_cliente"));
-				Cliente.setEmail_cliente(datos.getString("email_cliente"));
 				Cliente.setNombre_cliente(datos.getString("nombre_cliente"));
+				Cliente.setDireccion_cliente(datos.getString("direccion_cliente"));
 				Cliente.setTelefono_cliente(datos.getString("telefono_cliente"));
+				Cliente.setEmail_cliente(datos.getString("email_cliente"));
 				return Cliente;
 			}
 			Desconectar();
