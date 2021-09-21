765ce47eb1643c73a9f3a9928fc996b8c67e876e
Lauradlopezv04 <laurad.lopez0401@gmail.com>
2021-09-21 12:45:36 -0500

    Corrección clientes controller

--- TiendaDeportivaVirtual2/src/main/java/com/BO/TiendaDeportivaVirtual/ClientesController.java
+++ TiendaDeportivaVirtual2/src/main/java/com/BO/TiendaDeportivaVirtual/ClientesController.java
@@ -2,19 +2,19 @@ package com.BO.TiendaDeportivaVirtual;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.DAO.TiendaDeportivaVirtual.ClienteDao;
-import com.DTO.TiendaDeportivaVirtual.ClienteDao;
+import com.DTO.TiendaDeportivaVirtual.ClienteVo;
 
 @RestController
 public class ClientesController {
 /*Registrar Clientes*/
 	@RequestMapping("/registrarCliente")
-	public void registrarCliente(ClienteDao Cliente){
+	public void registrarCliente(ClienteVo Cliente){
 		ClienteDao Dao=new ClienteDao();
-		Dao.registrarCliente(Usuario);	
+		Dao.registrarCliente(Cliente);	
 }
 
   @RequestMapping("/actualizarCliente")
-	public void actualizarCliente(ClienteDao Cliente){
+	public void actualizarCliente(ClienteVo Cliente){
 		ClienteDao Dao=new ClienteDao();
 		Dao.actualizarCliente(Cliente);	
 	}
@@ -27,7 +27,7 @@ public class ClientesController {
 	}
 	
 	@RequestMapping("/consultarCliente")
-	public UsuarioVo consultarCliente(Long cedula_Cliente){
+	public ClienteVo consultarCliente(Long cedula_Cliente){
 		ClienteDao Dao= new ClienteDao();
 		return Dao.consultarCliente(cedula_Cliente);
 	}
