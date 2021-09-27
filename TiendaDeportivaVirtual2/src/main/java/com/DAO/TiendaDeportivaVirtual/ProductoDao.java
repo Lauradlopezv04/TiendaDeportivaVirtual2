94a7bc908e6d19470d57cc7d849485981e71cc71
Leal31 <lealemanuel31@protonmail.com>
2021-09-27 10:11:18 -0500

    leer

--- TiendaDeportivaVirtual2/src/main/java/com/DAO/TiendaDeportivaVirtual/ProductoDao.java
+++ TiendaDeportivaVirtual2/src/main/java/com/DAO/TiendaDeportivaVirtual/ProductoDao.java
@@ -1,9 +1,13 @@
 package com.DAO.TiendaDeportivaVirtual;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.DTO.TiendaDeportivaVirtual.ProductoVo;
 
@@ -39,6 +43,34 @@ public class ProductoDao extends Conexion {
 		}
 		} catch(Exception e) {
 			return false;
+		}
+	}
+	
+	public List<ProductoVo> leer() {
+		String archivo = "C:/Users/leale/OneDrive/Documentos/proyectos/misiontic/ciclo3/TiendaDeportivaVirtual2/TiendaDeportivaVirtual2/src/main/webapp/tablaprecios.csv";
+		BufferedReader entrada = null;
+		String linea = "";
+		String separar = ",";
+		List<ProductoVo> listadatos = new ArrayList<ProductoVo>();
+		ProductoVo producto = new ProductoVo();
+		try {
+			entrada = new BufferedReader(new FileReader(archivo));
+			while ((linea = entrada.readLine()) != null) {
+				
+				String[] datos = linea.split(separar);
+				producto.setCodigo_producto(Long.parseLong(datos[0]));
+				producto.setNombre_producto(datos[1]);
+				producto.setNitproveedor(Long.parseLong(datos[2]));
+				producto.setPrecio_compra(Double.parseDouble(datos[3]));
+				producto.setIvacompra(Long.parseLong(datos[4]));
+				producto.setPrecio_venta(Double.parseDouble(datos[5]));
+				listadatos.add(producto);
+				entrada.close();
+			}
+			return listadatos;
+		} catch(Exception e) {
+			System.out.println(e);
+			return null;
 		}
 	}
 }
