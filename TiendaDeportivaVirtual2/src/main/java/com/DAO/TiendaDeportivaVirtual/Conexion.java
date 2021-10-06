24621a0d67a29385492076e79c33dfc86a0048e6
Leal31 <lealemanuel31@protonmail.com>
2021-10-06 10:26:38 -0500

    corregir

--- TiendaDeportivaVirtual2/src/main/java/com/DAO/TiendaDeportivaVirtual/Conexion.java
+++ TiendaDeportivaVirtual2/src/main/java/com/DAO/TiendaDeportivaVirtual/Conexion.java
@@ -9,12 +9,8 @@ public class Conexion {
     protected void Conectar() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-<<<<<<< HEAD
-            //Conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/Tienda145?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8", "root", "root");
-            
-=======
-            Conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/tienda145?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8", "root", "root");
->>>>>>> cd878015ce1a3d944d8e27e7b634a645f4e1e8f4
+
+            Conexion = DriverManager.getConnection("jdbc:mysql://tiendavirtualjh.czo3ixoe3xoe.us-east-1.rds.amazonaws.com/tienda145?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8", "admin", "admin123");
             System.out.println("Conexión a base de datos OK\n");
         } catch (Exception e) {
             System.out.println("Error: " + e);
