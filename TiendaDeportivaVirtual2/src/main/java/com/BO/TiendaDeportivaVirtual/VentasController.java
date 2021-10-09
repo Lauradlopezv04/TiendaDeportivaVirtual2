d7c480bcfb6516f0418f7d082a3cf6ee601797bc
Leal31 <lealemanuel31@protonmail.com>
2021-10-09 15:05:44 -0500

    ya todo

--- TiendaDeportivaVirtual2/src/main/java/com/BO/TiendaDeportivaVirtual/VentasController.java
+++ TiendaDeportivaVirtual2/src/main/java/com/BO/TiendaDeportivaVirtual/VentasController.java
@@ -1,5 +1,22 @@
 package com.BO.TiendaDeportivaVirtual;
 
-public class VentasController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.DAO.TiendaDeportivaVirtual.VentasDao;
+import com.DTO.TiendaDeportivaVirtual.VentaVo;
+
+@RestController
+public class VentasController {
+	@RequestMapping("/guardarVenta")
+	public void guardarVenta(VentaVo Venta) {
+		VentasDao Dao = new VentasDao();
+		Dao.guardarVenta(Venta);
+	}
+	
+	@RequestMapping("guardarDetalleVenta")
+	public void guardardetalleVenta(VentaVo venta) {
+		VentasDao Dao = new VentasDao();
+		Dao.guardarDetalleventas(venta);
+	}
 }
